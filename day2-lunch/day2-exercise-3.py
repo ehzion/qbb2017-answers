e5cda4b2573169ae2863fe7ed97aa199ae578e02
Emily Zion <ezion1@jh.edu>
2017-08-29 13:06:30 -0400

    Python code for exercise 3

---
+++ day2-lunch/day2-exercise-3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+fh = sys.stdin
+
+total = 0
+for line in fh:
+    if "NH:i:1" in line:
+        total = total + 1
+    else:
+        continue
+    print total
