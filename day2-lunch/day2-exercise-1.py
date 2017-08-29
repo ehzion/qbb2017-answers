5f51fbbd97e6b228cd4cd1b1aa3701beeeca3d0e
Emily Zion <ezion1@jh.edu>
2017-08-29 12:48:21 -0400

    Python code for exercise 1

---
+++ day2-lunch/day2-exercise-1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+fh = sys.stdin
+
+total = 0
+for line in fh:
+    if line.startswith("@"):
+        continue
+    else:
+        total = total + 1
+    print total
