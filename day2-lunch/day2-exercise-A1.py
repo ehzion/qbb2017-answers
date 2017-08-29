a143ca407d0ae2adbe137728a835805dee1baf33
Emily Zion <ezion1@jh.edu>
2017-08-29 14:20:17 -0400

    Python code for advanced exercise 1

---
+++ day2-lunch/day2-exercise-A1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+fh = sys.stdin
+
+fwd = 0
+rev = 0
+        
+for line in fh:
+    if line.startswith("@"):
+        continue
+    else:
+        fields = line.split("\t")
+        if int(fields[1]) == 16:
+            rev = rev + 1
+        else:
+            fwd = fwd + 1
+print "Forward:" + str(fwd)
+print "Reverse:" + str(rev)
