e5f3a1915965071c613552bfe9d329da4f4cbcc4
Emily Zion <ezion1@jh.edu>
2017-08-29 19:14:59 -0400

    Python code for 2. Identifier mapping FINAL ANSWER

--- day2-homework/day2-mapping2.py
+++ day2-homework/day2-mapping2.py
@@ -3,6 +3,7 @@
 # Import sys
 import sys
 
+argument = sys.argv[2]
 # Use open to access the file from mapping 1 info
 f = open( sys.argv[1])
 # Use to create a dictionary for mapping 1 info
@@ -10,7 +11,8 @@ flydict = {}
 # Creating the dictionary
 for line in f:
     col = line.rstrip("\r\n").split("\t")
-    flybase = col[0]
+    # Had to delete an extra space that was introduced
+    flybase = col[0][:-1]
     ac = col[1]
     # Make sure every FlyBase ID is added to dictionary from mapping 1
     if flybase not in flydict:
@@ -30,11 +32,35 @@ for line in s:
     # Removes the header of ctab data
     if fields[8] == "gene_id":
         continue
-    # Searches for a match to FlyBase ID in ctab data in flydict
-    for fields[8] in flydict:
+    # Argument to skip lines
+    if argument == "skip":
+        # Finds FlyBase ID in flydict
+        if fields[8] in flydict:
+            # Stops printing after 100 lines of output
+            if count <= 100:
+                # Changes FlyBase ID to AC
+                fields[8] = flydict[fields[8]][0]
+                # Rejoins fields creating a new line with change above
+                print "\t".join(fields)
+                # Increases count to allow program to stop after 100 lines of output
+                count = count + 1
+    # Argument to replace with default value
+    if argument == "defv":
         # Stops printing after 100 lines of output
         if count <= 100:
-            # Prints the line corresponding to the matched FlyBase ID along with the AC, the corresponding values in the dictionary
-            print line.strip("\r\n"), "\t", flydict[fields[8]][0]
-            # Increases count to allow program to stop after 100 lines of output
-            count = count + 1
+            # Finds FlyBase ID in flydict
+            if fields[8] in flydict:
+                # Rejoins fields creating a new line with change above
+                fields[8] = flydict[fields[8]][0]
+                # Rejoins fields creating a new line with change above
+                print "\t".join(fields)
+                # Increases count to allow program to stop after 100 lines of output
+                count = count + 1
+            else:
+                # If FlyBase ID is not in flydict, replaces with N/A
+                fields[8] = "N/A"
+                # Rejoins fields creating a new line with change above
+                print "\t".join(fields)
+                # Increases count to allow program to stop after 100 lines of output
+                count = count + 1
+            
